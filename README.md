88151872e5d921e584f42e2d25d794c6c6b36921
kwong <jiangyanhao1991@gmail.com>
2022-04-16 22:09:27 +0800

    add: export private key script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # metaMaskTool
+## 批量导出metamask私钥
+1. 点击“账户” -> “展开详情”
+2. windows系统按F12，打开调试模式，点击“console”
+3. 更改 `outputPrivateKey.js` 里面的password为自己使用的password
+4. 把 `outputPrivateKey.js`的代码复制，回车即可自动下载私钥txt文件
+
+## 批量导入私钥到metamask
---
+++ outputPrivateKey.js
@@ -0,0 +1,90 @@
+const password = ''; // metamask passwrod
+
+function sleep(time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function download(filename, text) {
+  const element = document.createElement('a');
+  element.setAttribute(
+    'href',
+    'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
+  );
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+async function main() {
+  const keyList = [];
+  document.querySelector('.account-menu__icon').click();
+  const account = document.querySelectorAll(
+    '.account-menu__account.account-menu__item--clickable'
+  );
+  for (let i = 0; i < account.length; i++) {
+    console.log(i);
+    if (
+      !document.querySelector(
+        '.account-menu__account.account-menu__item--clickable'
+      )
+    ) {
+      document.querySelector('.account-menu__icon').click();
+    }
+    await sleep(100);
+    const accountList = document.querySelectorAll(
+      '.account-menu__account.account-menu__item--clickable'
+    );
+    // console.log(accountList[i]);
+    accountList[i].click();
+    await sleep(100);
+    document
+      .querySelector('.fas.fa-ellipsis-v.menu-bar__account-options')
+      .click();
+    document.querySelector('button[class="menu-item"]:nth-child(2)').click();
+    await sleep(100);
+    document
+      .querySelectorAll(
+        '.button.btn--rounded.btn-secondary.account-details-modal__button'
+      )[1]
+      .click();
+    await sleep(100);
+    function setNativeValue(element, value) {
+      const valueSetter = Object.getOwnPropertyDescriptor(element, 'value').set;
+      const prototype = Object.getPrototypeOf(element);
+      const prototypeValueSetter = Object.getOwnPropertyDescriptor(
+        prototype,
+        'value'
+      ).set;
+
+      if (valueSetter && valueSetter !== prototypeValueSetter) {
+        prototypeValueSetter.call(element, value);
+      } else {
+        valueSetter.call(element, value);
+      }
+    }
+    const t = document.querySelector(
+      '.export-private-key-modal__password-input'
+    );
+    setNativeValue(t, password);
+    t.dispatchEvent(new Event('input', { bubbles: true }));
+    await sleep(100);
+    document
+      .querySelector(
+        '.button.btn--rounded.btn-primary.btn--large.export-private-key-modal__button'
+      )
+      .click();
+    await sleep(100);
+    const key = document.querySelector('textarea').value;
+    // console.log(key);
+    keyList.push(key);
+  }
+  // console.log(keyList);
+  download('key.txt', JSON.stringify(keyList));
+}
+
+main();
